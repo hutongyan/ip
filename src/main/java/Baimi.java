004877a2daa9e13d6024a533d55764a8db0a862b
tongytan hu <hu_tongyan@u.nus.edu>
2025-01-23 19:14:37 +0800

    A-Enums: using enums in TaskType.

--- src/main/java/Baimi.java
+++ src/main/java/Baimi.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Baimi {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
