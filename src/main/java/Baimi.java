316b7a6077d66c43447c99438ed4cc8f69bec359
tongytan hu <hu_tongyan@u.nus.edu>
2025-01-23 14:51:32 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Baimi.java
@@ -0,0 +1,12 @@
+public class Baimi {
+    public static void main(String[] args) {
+        String logo = " ____    _    _   __  __   _\n"
+                + "| __ )  / \\  | | |  \\/  | (_)\n"
+                + "|  _ \\ / _ \\ | | | |\\/| | | |\n"
+                + "| |_) / ___ \\| | | |  | | | |\n"
+                + "|____/_/   \\_\\_| |_|  |_| |_|\n";
+        System.out.println("Hello! I'm Baimi\n" + logo);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
