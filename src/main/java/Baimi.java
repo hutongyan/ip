83b04ee1863f88c95d85a4401152483d22abde22
tongytan hu <hu_tongyan@u.nus.edu>
2025-01-23 16:18:00 +0800

    Level-2: Add, List

--- src/main/java/Baimi.java
+++ src/main/java/Baimi.java
@@ -13,22 +13,37 @@ public class Baimi {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String command;
 
         while (true) {
             command = scanner.nextLine();
-            System.out.println("____________________________________________________________");
-            System.out.println(command);
             System.out.println("____________________________________________________________");
 
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (command.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println("No tasks available.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("added: " + command);
             }
+
+            System.out.println("____________________________________________________________");
         }
 
         scanner.close();
     }
 }
+
 
